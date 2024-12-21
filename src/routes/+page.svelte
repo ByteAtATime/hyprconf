7a1fa139ddb854f3097f88110336290b0ba437bb
ByteAtATime <byteatatime@proton.me>
2024-12-21 13:35:38 -0800

    chore: formatting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,41 +1,41 @@
 <script lang="ts">
-  import { invoke } from "@tauri-apps/api/core";
-  import { Button } from "$lib/components/ui/button";
-  import { Command } from '@tauri-apps/plugin-shell';
+	import { invoke } from '@tauri-apps/api/core';
+	import { Button } from '$lib/components/ui/button';
+	import { Command } from '@tauri-apps/plugin-shell';
 
-  let name = $state("");
-  let greetMsg = $state("");
+	let name = $state('');
+	let greetMsg = $state('');
 
-  async function greet(event: Event) {
-    event.preventDefault();
+	async function greet(event: Event) {
+		event.preventDefault();
 
-    let result = await Command.create('hyprctl', ["monitors"]).execute();
-    console.log(result);
+		let result = await Command.create('hyprctl', ['monitors']).execute();
+		console.log(result);
 
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    greetMsg = await invoke("greet", { name });
-  }
+		// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+		greetMsg = await invoke('greet', { name });
+	}
 </script>
 
 <main class="container">
-  <h1>Welcome to Tauri + Svelte</h1>
+	<h1>Welcome to Tauri + Svelte</h1>
 
-  <div class="row">
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo vite" alt="Vite Logo" />
-    </a>
-    <a href="https://tauri.app" target="_blank">
-      <img src="/tauri.svg" class="logo tauri" alt="Tauri Logo" />
-    </a>
-    <a href="https://kit.svelte.dev" target="_blank">
-      <img src="/svelte.svg" class="logo svelte-kit" alt="SvelteKit Logo" />
-    </a>
-  </div>
-  <p>Click on the Tauri, Vite, and SvelteKit logos to learn more.</p>
+	<div class="row">
+		<a href="https://vitejs.dev" target="_blank">
+			<img src="/vite.svg" class="logo vite" alt="Vite Logo" />
+		</a>
+		<a href="https://tauri.app" target="_blank">
+			<img src="/tauri.svg" class="logo tauri" alt="Tauri Logo" />
+		</a>
+		<a href="https://kit.svelte.dev" target="_blank">
+			<img src="/svelte.svg" class="logo svelte-kit" alt="SvelteKit Logo" />
+		</a>
+	</div>
+	<p>Click on the Tauri, Vite, and SvelteKit logos to learn more.</p>
 
-  <form class="row" onsubmit={greet}>
-    <input id="greet-input" placeholder="Enter a name..." bind:value={name} />
-    <Button type="submit">Greet</Button>
-  </form>
-  <p>{greetMsg}</p>
+	<form class="row" onsubmit={greet}>
+		<input id="greet-input" placeholder="Enter a name..." bind:value={name} />
+		<Button type="submit">Greet</Button>
+	</form>
+	<p>{greetMsg}</p>
 </main>
